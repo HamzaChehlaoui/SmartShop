52577d554e057c2ce822effe49f6682a3d4850f3
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-26 11:49:19 +0100

    edit class implimentation service de authentication

--- src/main/java/com/microtech/smartshop/service/AuthenticationServiceImpl.java
+++ src/main/java/com/microtech/smartshop/service/AuthenticationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.microtech.smartshop.service;
 
+import com.microtech.smartshop.config.SessionInterceptor;
 import com.microtech.smartshop.dto.request.ClientCreateRequestDTO;
 import com.microtech.smartshop.dto.request.UserCreateRequestDTO;
 import com.microtech.smartshop.dto.response.UserResponseDTO;
@@ -14,7 +15,6 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -31,7 +31,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public UserResponseDTO register(UserCreateRequestDTO request, HttpSession session) {
 
         if (userRepository.findByUsername(request.getUsername()).isPresent()) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Username already exists");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username already exists");
         }
 
         String hashedPassword = BCrypt.hashpw(request.getPassword(), BCrypt.gensalt());
@@ -59,26 +59,28 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         userRepository.save(user);
 
-        session.setAttribute("userId", user.getId());
+        SessionInterceptor.setLoggedUser(session, user);
 
         return userMapper.toResponseDTO(user);
     }
 
-
     @Override
-    public UserResponseDTO login(UserCreateRequestDTO request) {
+    public UserResponseDTO login(UserCreateRequestDTO request, HttpSession session) {
         User user = userRepository.findByUsername(request.getUsername())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Invalid username or password"));
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid username or password"));
 
         if (!BCrypt.checkpw(request.getPassword(), user.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Invalid username or password");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid username or password");
         }
+
+        SessionInterceptor.setLoggedUser(session, user);
 
         return userMapper.toResponseDTO(user);
     }
 
     @Override
     public void logout(HttpSession session) {
-        session.invalidate();
+        SessionInterceptor.clearSession(session);
     }
 }
