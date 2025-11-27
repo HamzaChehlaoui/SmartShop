ccf563547d07d3ae1998a24563a3ea0d42896205
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-27 15:16:29 +0100

    ajouter interface produit service

--- src/main/java/com/microtech/smartshop/service/ProductService.java
+++ src/main/java/com/microtech/smartshop/service/ProductService.java
@@ -1,4 +1,15 @@
 package com.microtech.smartshop.service;
 
-public class ProductService {
+import com.microtech.smartshop.dto.request.ProductCreateRequest;
+import com.microtech.smartshop.dto.request.ProductUpdateRequest;
+import com.microtech.smartshop.dto.response.ProductResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+public interface ProductService {
+    ProductResponse createProduct(ProductCreateRequest req);
+    ProductResponse updateProduct(Long id, ProductUpdateRequest req);
+    ResponseEntity<String> deleteProduct(Long id);
+    Page<ProductResponse> listProducts(String nomFilter, int page, int size, String sortBy, String direction);
+    ProductResponse getProduct(Long id);
 }
