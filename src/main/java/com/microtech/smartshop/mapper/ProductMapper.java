ff0f0dd5dd0b2024d7e7c7e7e2c07d8a97f13227
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-27 15:14:03 +0100

    ajouter produit mapper

--- src/main/java/com/microtech/smartshop/mapper/ProductMapper.java
+++ src/main/java/com/microtech/smartshop/mapper/ProductMapper.java
@@ -1,4 +1,23 @@
 package com.microtech.smartshop.mapper;
 
-public class ProductMapper {
+import com.microtech.smartshop.dto.request.ProductCreateRequest;
+import com.microtech.smartshop.dto.request.ProductUpdateRequest;
+import com.microtech.smartshop.dto.response.ProductResponse;
+import com.microtech.smartshop.entity.Product;
+import org.mapstruct.*;
+import java.util.*;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    Product toEntity(ProductCreateRequest dto);
+
+    Product toEntity(ProductUpdateRequest dto);
+
+    ProductResponse toResponse(Product entity);
+
+    List<ProductResponse> toResponseList(List<Product> entities);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateProductFromDto(ProductUpdateRequest dto, @MappingTarget Product entity);
 }
