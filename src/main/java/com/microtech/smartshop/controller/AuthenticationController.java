86620fcb95bc60cc9a245f130a552d6584f0abc9
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-27 11:16:30 +0100

    j'ai ahoute controller de authentication

---
+++ src/main/java/com/microtech/smartshop/controller/AuthenticationController.java
@@ -0,0 +1,46 @@
+package com.microtech.smartshop.controller;
+
+import com.microtech.smartshop.dto.request.UserCreateRequestDTO;
+import com.microtech.smartshop.dto.response.UserResponseDTO;
+import com.microtech.smartshop.service.AuthenticationService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(
+            @Valid @RequestBody UserCreateRequestDTO request,
+            HttpSession session) {
+
+        UserResponseDTO response = authenticationService.register(request, session);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponseDTO> login(@Valid @RequestBody UserCreateRequestDTO request,
+                                                 HttpSession session) {
+
+        UserResponseDTO response = authenticationService.login(request);
+
+        session.setAttribute("userId", response.getId());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpSession session) {
+        authenticationService.logout(session);
+        return ResponseEntity.ok("Logged out successfully");
+    }
+}
