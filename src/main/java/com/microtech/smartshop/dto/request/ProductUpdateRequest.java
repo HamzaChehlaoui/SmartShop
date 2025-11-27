1116197330edcd9bfc78af4e53fbcea614fe17b5
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-27 15:10:45 +0100

    ajouter produit update request dto

--- src/main/java/com/microtech/smartshop/dto/request/ProductUpdateRequest.java
+++ src/main/java/com/microtech/smartshop/dto/request/ProductUpdateRequest.java
@@ -1,4 +1,21 @@
 package com.microtech.smartshop.dto.request;
 
+import lombok.*;
+import java.math.BigDecimal;
+import jakarta.validation.constraints.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductUpdateRequest {
+    @NotBlank
+    private String nom;
+
+    @NotNull
+    @DecimalMin("0.0")
+    private BigDecimal prixHt;
+
+    @NotNull
+    @Min(0)
+    private Integer stock;
 }
