6b6e88de9b672eacae80567a3ad37d3afaa420e6
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-27 15:11:58 +0100

    ajouter produit response dto

--- src/main/java/com/microtech/smartshop/dto/response/ProductResponse.java
+++ src/main/java/com/microtech/smartshop/dto/response/ProductResponse.java
@@ -1,4 +1,15 @@
 package com.microtech.smartshop.dto.response;
 
+import lombok.*;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductResponse {
+    private Long id;
+    private String nom;
+    private BigDecimal prixHt;
+    private Integer stock;
 }
