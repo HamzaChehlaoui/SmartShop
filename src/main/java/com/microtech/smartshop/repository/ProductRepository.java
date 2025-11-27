5f6930bae6e41e82a534454680ee75bbb0c749d2
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-27 15:15:35 +0100

    ajouter repository  produit

--- src/main/java/com/microtech/smartshop/repository/ProductRepository.java
+++ src/main/java/com/microtech/smartshop/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.microtech.smartshop.repository;
 
-public interface ProductRepository {
+import com.microtech.smartshop.entity.Product;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByDeletedFalse(Pageable pageable);
+    Page<Product> findByDeletedFalseAndNomContainingIgnoreCase(String nom, Pageable pageable);
 }
