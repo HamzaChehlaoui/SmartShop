66c1c5a585df9fa228c32dce0b0a9d3c5f62fbad
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-28 11:33:33 +0100

    Créez interface service  pour las commades

--- src/main/java/com/microtech/smartshop/service/CommandeService.java
+++ src/main/java/com/microtech/smartshop/service/CommandeService.java
@@ -1,4 +1,16 @@
 package com.microtech.smartshop.service;
 
-public class CommandeService {
+import com.microtech.smartshop.dto.request.CommandeCreateRequest;
+import com.microtech.smartshop.dto.response.CommandeResponse;
+import com.microtech.smartshop.enums.OrderStatus;
+
+public interface CommandeService {
+
+    CommandeResponse createCommande(CommandeCreateRequest request);
+
+    CommandeResponse getCommande(Long id);
+
+    CommandeResponse updateCommandeStatus(Long id, OrderStatus newStatus);
+
+    void cancelCommande(Long id);
 }
