40bdb803dd048d6d462dd69ac901b9db2c63ba49
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-28 11:30:33 +0100

    Créez mapper  pour las commades

--- src/main/java/com/microtech/smartshop/mapper/CommandeMapper.java
+++ src/main/java/com/microtech/smartshop/mapper/CommandeMapper.java
@@ -1,4 +1,17 @@
 package com.microtech.smartshop.mapper;
 
-public class CommandeMapper {
+import com.microtech.smartshop.dto.response.CommandeHistoryResponse;
+import com.microtech.smartshop.dto.response.CommandeResponse;
+import com.microtech.smartshop.entity.Commande;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", uses = { OrderItemMapper.class, PaymentMapper.class })
+public interface CommandeMapper {
+
+    @Mapping(source = "client.id", target = "clientId")
+    @Mapping(source = "client.name", target = "clientName")
+    @Mapping(source = "orderItems", target = "items")
+    CommandeResponse toResponse(Commande commande);
+
+    CommandeHistoryResponse toHistoryResponse(Commande commande);
 }
