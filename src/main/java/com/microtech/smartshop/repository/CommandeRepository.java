13aee25d14f78159478732683c6ea9d1c337f707
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-28 11:31:39 +0100

    Créez repository  pour las commades

--- src/main/java/com/microtech/smartshop/repository/CommandeRepository.java
+++ src/main/java/com/microtech/smartshop/repository/CommandeRepository.java
@@ -1,4 +1,18 @@
 package com.microtech.smartshop.repository;
 
-public interface CommandeRepository {
+import com.microtech.smartshop.entity.Commande;
+import com.microtech.smartshop.enums.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommandeRepository extends JpaRepository<Commande, Long> {
+
+    List<Commande> findByClientIdOrderByDateCreationDesc(Long clientId);
+
+    List<Commande> findByClientIdAndStatus(Long clientId, OrderStatus status);
+
+    long countByClientIdAndStatus(Long clientId, OrderStatus status);
 }
