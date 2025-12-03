fa58e8f2b5e4f453c6271dd23ba27a5f4132f5fc
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-03 15:29:55 +0100

    ajout payment mapper

--- src/main/java/com/microtech/smartshop/mapper/PaymentMapper.java
+++ src/main/java/com/microtech/smartshop/mapper/PaymentMapper.java
@@ -1,4 +1,12 @@
 package com.microtech.smartshop.mapper;
 
-public class PaymentMapper {
+import com.microtech.smartshop.dto.response.PaymentResponse;
+import com.microtech.smartshop.entity.Payment;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper {
+
+    @Mapping(source = "commande.id", target = "commandeId")
+    PaymentResponse toResponse(Payment payment);
 }
