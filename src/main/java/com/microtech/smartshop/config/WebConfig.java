09f6a58b22990fb763ff7fced0c3661e1d820c31
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2026-01-29 09:01:18 +0000

    Implement CORS configuration and refine interceptor path patterns

--- src/main/java/com/microtech/smartshop/config/WebConfig.java
+++ src/main/java/com/microtech/smartshop/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.microtech.smartshop.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +15,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(sessionInterceptor)
-                .addPathPatterns("/api/**");
+                .addPathPatterns("/api/**")
+                ;
     }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+
 }
